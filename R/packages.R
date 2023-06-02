ec56e21f442c666f4dd7a0b30ec6ef20c5d0c141
JSartini <55293050+JSartini@users.noreply.github.com>
2023-06-01 22:18:37 -0400

    Updated to correctly handle current package dependencies

--- R/packages.R
+++ R/packages.R
@@ -1,2 +1,6 @@
-#' @importFrom stats spec.pgram
+#' @importFrom stats spec.pgram lm predict arima.sim median
+#' @importFrom magrittr %>%
+#' @importFrom dplyr slice filter
+#' @importFrom readr read_csv
+#' @importFrom utils tail
 NULL
