fca2af138e3eb995b63db9ab8125f4df7881cf10
JSartini <55293050+JSartini@users.noreply.github.com>
2023-06-02 11:14:10 -0400

    Updated 6DF calculations to fix erroneous indexing and naming issues

--- R/Metrics.R
+++ R/Metrics.R
@@ -16,31 +16,34 @@
 #'
 linear_approx <- function(pgram, freqs){
   fit_df = data.frame(spectra = pgram, frequencies = freqs, periods = 1/freqs)
-  long_idxs = which(periods >= 1440)
-  int_idxs = which(periods < 1440 & periods >= 250)
-  short_idxs = which(periods < 250)
+  long_idxs = which(fit_df$periods >= 1440)
+  short_idxs = which(fit_df$periods < 150)
+  int_idxs = c(long_idxs[length(long_idxs)],
+               which(fit_df$periods < 1440 & fit_df$periods >= 150),
+               short_idxs[1])
 
-  long_inputs = fit_df %>% slice(long_idxs)
+  long_inputs = fit_df[long_idxs,]
   mid = mean(long_inputs$frequencies)
   long_fit = lm(spectra ~ frequencies, data = long_inputs)
   lslope = long_fit$coefficients[2]
   lmidpoint = predict(long_fit, newdata = data.frame(frequencies = c(mid)))
 
-  med_inputs = fit_df %>% slice(int_idxs)
+  med_inputs = fit_df[int_idxs,]
   mid = median(med_inputs$frequencies)
   med_fit = lm(spectra ~ frequencies, data = med_inputs)
   islope = med_fit$coefficients[2]
   imidpoint = predict(med_fit, newdata = data.frame(frequencies = c(mid)))
 
-  short_inputs = fit_df %>% slice(short_idxs)
+  short_inputs = fit_df[short_idxs,]
   mid = mean(short_inputs$frequencies)
   short_fit = lm(spectra ~ frequencies, data = short_inputs)
   sslope = short_fit$coefficients[2]
   smidpoint = predict(short_fit, newdata = data.frame(frequencies = c(mid)))
 
-  return(c(long_slope = lslope, long_midpoint = lmidpoint,
-           int_slope = islope, int_midpoint = imidpoint,
-           short_slope = sslope, short_midpoint = smidpoint))
+  output = unname(c(lslope, lmidpoint, islope, imidpoint, sslope, smidpoint))
+  names(output) = c("long_slope", "long_midpoint", "int_slope", "int_midpoint",
+                    "short_slope", "short_midpoint")
+  return(output)
 }
 
 
