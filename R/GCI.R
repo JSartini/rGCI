dd9c390d0473ab150a58c3976cc6c670235454cd
JSartini <jsartin1@jhu.edu>
2024-01-20 11:16:56 -0500

    Created generic method for calculate_GCI, allowing it to be used for both 6DF_summary and dataframes

--- R/GCI.R
+++ R/GCI.R
@@ -16,12 +16,10 @@ default_weights <- function(){
 #' @details Using weights, defaulting to those derived from analysis of the repeat
 #' CGM wears in the HYPNOS trial, generates the GCI as a weighted linear combination
 #' of the 6 degree of freedom disjoint piece-wise linear approximation of the
-#' log-periodogram. All vector arguments should be named using the standard
-#' convention for these metrics, which is period_metric for periods of long, int,
-#' and short, metrics of slope and intercept.
+#' log-periodogram.
 #'
-#' @param linear_approx, object of class "6DF_summary" containing 6-DF
-#' representation of log-periodogram
+#' @param x, dataframe with raw CGM time series or object of class "6DF_summary"
+#' containing 6-DF representation of log-periodogram
 #' @param weights, numeric vector of weights used to form the GCI linear combination
 #' @param means, numeric vector of mean values for the 6 degree of freedom
 #' approximation, subtracted away before calculation of GCI
@@ -32,7 +30,30 @@ default_weights <- function(){
 #'
 #' @export
 #'
-calculate_GCI <- function(linear_approx, weights = default_weights(),
+calculate_GCI <- function(x, weights, means, stddevs) UseMethod("calculate_GCI")
+
+
+#' Calculates Glucose Color Index (GCI) using 6DF disjoint linear summary of
+#' log-periodogram
+#'
+#' @details Using weights, defaulting to those derived from analysis of the repeat
+#' CGM wears in the HYPNOS trial, generates the GCI as a weighted linear combination
+#' of a provided 6 degree of freedom disjoint piece-wise linear approximation of
+#' the log-periodogram.
+#'
+#' @param x, Object of class "6DF_summary" containing 6-DF representation of
+#' log-periodogram
+#' @param weights, numeric vector of weights used to form the GCI linear combination
+#' @param means, numeric vector of mean values for the 6 degree of freedom
+#' approximation, subtracted away before calculation of GCI
+#' @param stddevs, numeric vector of standard deviations for each of the values
+#' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#'
+#' @return gci, numeric containing the Glucose Color Index variability metric value
+#'
+#' @export
+#'
+calculate_GCI.6DF_summary <- function(x, weights = default_weights(),
                           means = c(long_slope = 0, long_midpoint = 0,
                                     int_slope = 0, int_midpoint = 0,
                                     short_slope = 0, short_midpoint = 0),
@@ -47,8 +68,25 @@ calculate_GCI <- function(linear_approx, weights = default_weights(),
   }
 
   result = 0
-  for(dfmet in names(linear_approx)){
-    result = result + (linear_approx[[dfmet]] - means[dfmet])/stddevs[dfmet] * weights[dfmet]
+  for(dfmet in names(x)){
+    result = result + (x[[dfmet]] - means[dfmet])/stddevs[dfmet] * weights[dfmet]
   }
   return(unname(result))
+}
+
+
+
+#' Catch attempt to calculate GCI using unsupported class
+#'
+#' @param x, Object of class other than "6DF_summary" or dataframe
+#' @param weights, numeric vector of weights used to form the GCI linear combination
+#' @param means, numeric vector of mean values for the 6 degree of freedom
+#' approximation, subtracted away before calculation of GCI
+#' @param stddevs, numeric vector of standard deviations for each of the values
+#' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#'
+#' @export
+#'
+calculate_GCI.default <- function(x, weights, means, stddevs){
+  stop("GCI calculation not yet supported for this object type")
 }
