169a28336190dfbd83af2b9af4b9bb37f2b9606b
JSartini <jsartin1@jhu.edu>
2024-01-21 16:24:47 -0500

    Implemented calculation of GCI using dataframe with CGM time series in long format (with tests)

--- R/GCI.R
+++ R/GCI.R
@@ -25,12 +25,13 @@ default_weights <- function(){
 #' approximation, subtracted away before calculation of GCI
 #' @param stddevs, numeric vector of standard deviations for each of the values
 #' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#' @param ..., additional arguments/settings required
 #'
 #' @return gci, numeric containing the Glucose Color Index variability metric value
 #'
 #' @export
 #'
-calculate_GCI <- function(x, weights, means, stddevs) UseMethod("calculate_GCI")
+calculate_GCI <- function(x, weights, means, stddevs, ...) UseMethod("calculate_GCI")
 
 
 #' Calculates Glucose Color Index (GCI) using 6DF disjoint linear summary of
@@ -48,6 +49,7 @@ calculate_GCI <- function(x, weights, means, stddevs) UseMethod("calculate_GCI")
 #' approximation, subtracted away before calculation of GCI
 #' @param stddevs, numeric vector of standard deviations for each of the values
 #' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#' @param ..., additional arguments/settings (not required here)
 #'
 #' @return gci, numeric containing the Glucose Color Index variability metric value
 #'
@@ -59,7 +61,8 @@ calculate_GCI.6DF_summary <- function(x, weights = default_weights(),
                                     short_slope = 0, short_midpoint = 0),
                           stddevs = c(long_slope = 1, long_midpoint = 1,
                                       int_slope = 1, int_midpoint = 1,
-                                      short_slope = 1, short_midpoint = 1)){
+                                      short_slope = 1, short_midpoint = 1),
+                          ...){
   reqnames = c("long_slope", "long_midpoint", "int_slope", "int_midpoint",
                "short_slope", "short_midpoint")
 
@@ -74,7 +77,56 @@ calculate_GCI.6DF_summary <- function(x, weights = default_weights(),
   return(unname(result))
 }
 
+#' Calculates Glucose Color Index (GCI) using long-format raw CGM time series
+#' in a dataframe with sensible defaults
+#'
+#' @details Using weights, defaulting to those derived from analysis of the repeat
+#' CGM wears in the HYPNOS trial, generates the GCI as a weighted linear combination
+#' of a provided 6 degree of freedom disjoint piece-wise linear approximation of
+#' the log-periodogram. If columnnames are not properly specified, an error will
+#' be thrown.
+#'
+#' @param x, dataframe containing time argument in "timestamp" column
+#' and CGM argument in "glucose" column
+#' @param weights, numeric vector of weights used to form the GCI linear combination
+#' @param means, numeric vector of mean values for the 6 degree of freedom
+#' approximation, subtracted away before calculation of GCI
+#' @param stddevs, numeric vector of standard deviations for each of the values
+#' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#' @param ..., can include list of periodogram options: number of expected days
+#' in the data ("days"), maximum gap in the data allowable ("maxgap", in days),
+#' minimum data for analysis ("mindata" in days), amount of start and end data to
+#' trim ("strim" and "etrim" both in days), and odd-value spans of Daniel smoothing
+#' kernels ("spans"). This argument is required, and a set of reasonable
+#' defaults is provided by the default_pgram() function.
+#'
+#' @return gci, numeric containing the Glucose Color Index variability metric value
+#'
+#' @export
+#'
+calculate_GCI.data.frame <- function(x, weights = default_weights(),
+                                      means = c(long_slope = 0, long_midpoint = 0,
+                                                int_slope = 0, int_midpoint = 0,
+                                                short_slope = 0, short_midpoint = 0),
+                                      stddevs = c(long_slope = 1, long_midpoint = 1,
+                                                  int_slope = 1, int_midpoint = 1,
+                                                  short_slope = 1, short_midpoint = 1), ...){
+  reqnames = c("long_slope", "long_midpoint", "int_slope", "int_midpoint",
+               "short_slope", "short_midpoint")
 
+  if(!all(reqnames %in% names(means)) | !all(reqnames %in% names(stddevs))){
+    stop("Insufficient specification for standardization")
+  }
+
+ if(length(list(...))>0){
+   est_lpgram = pgram(x$glucose, x$timestamp, ...)
+ }
+ else{
+   est_lpgram = pgram(x$glucose, x$timestamp)
+ }
+ summary_6DF = linear_approx(est_lpgram)
+ calculate_GCI(summary_6DF, weights, means, stddevs)
+}
 
 #' Catch attempt to calculate GCI using unsupported class
 #'
@@ -84,9 +136,10 @@ calculate_GCI.6DF_summary <- function(x, weights = default_weights(),
 #' approximation, subtracted away before calculation of GCI
 #' @param stddevs, numeric vector of standard deviations for each of the values
 #' within the 6 degree of freedom approximation, used for scaling before GCI calculation
+#' @param ..., additional arguments/settings (not required here)
 #'
 #' @export
 #'
-calculate_GCI.default <- function(x, weights, means, stddevs){
+calculate_GCI.default <- function(x, weights, means, stddevs, ...){
   stop("GCI calculation not yet supported for this object type")
 }
--- tests/testthat/test-GCI.R
+++ tests/testthat/test-GCI.R
@@ -1,3 +1,53 @@
+test_that("user can pass periodogram arguments to GCI calculation procedure", {
+  baseline = baseline_metrics %>%
+    filter(ID == "full_ma") %>%
+    select(GCI) %>%
+    as.numeric() %>%
+    unname()
+
+  # Stddevs accommodate for frequency unit shift in old implementation
+  calculated = calculate_GCI(data.frame(timestamp = full_times, glucose = full_data_ma1),
+                             weights = default_weights(),
+                             means = c(long_slope = 0, long_midpoint = 0,
+                                       int_slope = 0, int_midpoint = 0,
+                                       short_slope = 0, short_midpoint = 0),
+                             stddevs = c(long_slope = 15, long_midpoint = 1,
+                                         int_slope = 15, int_midpoint = 1,
+                                         short_slope = 15, short_midpoint = 1),
+                             default_pgram())
+  expect_equal(calculated, baseline)
+})
+
+test_that("GCI on DF matches previous implementation: MA(1)", {
+  baseline = baseline_metrics %>%
+    filter(ID == "full_ma") %>%
+    select(GCI) %>%
+    as.numeric() %>%
+    unname()
+
+  # Stddevs accommodate for frequency unit shift in old implementation
+  calculated = calculate_GCI(data.frame(timestamp = full_times, glucose = full_data_ma1),
+                             stddevs = c(long_slope = 15, long_midpoint = 1,
+                                         int_slope = 15, int_midpoint = 1,
+                                         short_slope = 15, short_midpoint = 1))
+  expect_equal(calculated, baseline)
+})
+
+test_that("GCI on DF matches previous implementation: AR(1)", {
+  baseline = baseline_metrics %>%
+    filter(ID == "full_ar") %>%
+    select(GCI) %>%
+    as.numeric() %>%
+    unname()
+
+  # Stddevs accommodate for frequency unit shift in old implementation
+  calculated = calculate_GCI(data.frame(timestamp = full_times, glucose = full_data_ar1),
+                             stddevs = c(long_slope = 15, long_midpoint = 1,
+                                         int_slope = 15, int_midpoint = 1,
+                                         short_slope = 15, short_midpoint = 1))
+  expect_equal(calculated, baseline)
+})
+
 test_that("GCI matches previous implementation: MA(1)", {
   baseline = baseline_metrics %>%
     filter(ID == "full_ma") %>%
@@ -6,6 +56,24 @@ test_that("GCI matches previous implementation: MA(1)", {
     unname()
 
   fit = pgram(full_data_ma1, full_times)
+  metrics = linear_approx(fit)
+  # Accommodates for frequency unit shift in old implementation
+  metrics$long_slope = metrics$long_slope/15
+  metrics$int_slope = metrics$int_slope/15
+  metrics$short_slope = metrics$short_slope/15
+  calculated = calculate_GCI(metrics)
+
+  expect_equal(calculated, baseline)
+})
+
+test_that("GCI matches previous implementation: AR(1)", {
+  baseline = baseline_metrics %>%
+    filter(ID == "full_ar") %>%
+    select(GCI) %>%
+    as.numeric() %>%
+    unname()
+
+  fit = pgram(full_data_ar1, full_times)
   metrics = linear_approx(fit)
   # Accommodates for frequency unit shift in old implementation
   metrics$long_slope = metrics$long_slope/15
