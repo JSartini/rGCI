b33b045641bf224819df58eaf74d2227b8be4b91
JSartini <jsartin1@jhu.edu>
2024-01-21 16:28:11 -0500

    Updated tests to more appropriately use arguments from calculate_GCI()

--- tests/testthat/test-GCI.R
+++ tests/testthat/test-GCI.R
@@ -57,11 +57,11 @@ test_that("GCI matches previous implementation: MA(1)", {
 
   fit = pgram(full_data_ma1, full_times)
   metrics = linear_approx(fit)
-  # Accommodates for frequency unit shift in old implementation
-  metrics$long_slope = metrics$long_slope/15
-  metrics$int_slope = metrics$int_slope/15
-  metrics$short_slope = metrics$short_slope/15
-  calculated = calculate_GCI(metrics)
+  # Stddevs accommodate for frequency unit shift in old implementation
+  calculated = calculate_GCI(metrics,
+                             stddevs = c(long_slope = 15, long_midpoint = 1,
+                                         int_slope = 15, int_midpoint = 1,
+                                         short_slope = 15, short_midpoint = 1))
 
   expect_equal(calculated, baseline)
 })
@@ -75,11 +75,11 @@ test_that("GCI matches previous implementation: AR(1)", {
 
   fit = pgram(full_data_ar1, full_times)
   metrics = linear_approx(fit)
-  # Accommodates for frequency unit shift in old implementation
-  metrics$long_slope = metrics$long_slope/15
-  metrics$int_slope = metrics$int_slope/15
-  metrics$short_slope = metrics$short_slope/15
-  calculated = calculate_GCI(metrics)
+  # Stddevs accommodate for frequency unit shift in old implementation
+  calculated = calculate_GCI(metrics,
+                             stddevs = c(long_slope = 15, long_midpoint = 1,
+                                         int_slope = 15, int_midpoint = 1,
+                                         short_slope = 15, short_midpoint = 1))
 
   expect_equal(calculated, baseline)
 })
@@ -87,19 +87,13 @@ test_that("GCI matches previous implementation: AR(1)", {
 test_that("GCI throws error without sufficient standardization mean elements", {
   fit = pgram(full_data_ar1, full_times)
   metrics = linear_approx(fit)
-  # Accommodates for frequency unit shift in old implementation
-  metrics$long_slope = metrics$long_slope/15
-  metrics$int_slope = metrics$int_slope/15
-  metrics$short_slope = metrics$short_slope/15
+
   expect_error(calculate_GCI(metrics, means = c(long_slope = 0, long_midpoint = 0)))
 })
 
 test_that("GCI throws error without sufficient standardization SD elements", {
   fit = pgram(full_data_ar1, full_times)
   metrics = linear_approx(fit)
-  # Accommodates for frequency unit shift in old implementation
-  metrics$long_slope = metrics$long_slope/15
-  metrics$int_slope = metrics$int_slope/15
-  metrics$short_slope = metrics$short_slope/15
+
   expect_error(calculate_GCI(metrics, stddevs = c(long_slope = 0, long_midpoint = 0)))
 })
